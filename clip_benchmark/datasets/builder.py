52a903f5bac43520e1d4d64de2476f3345b10806
shaun <shaun@quadrical.ai>
2023-07-26 19:22:33 +0200

    added corruptions

--- clip_benchmark/datasets/builder.py
+++ clip_benchmark/datasets/builder.py
@@ -384,9 +384,10 @@ def build_dataset(dataset_name, root="root", transform=None, split="test", downl
     elif dataset_name == "pascal3d":
         ds = pascal3d.Pascal3d(root=root, target_type="category", transform=transform, download=False,
                                split=split, **kwargs)
-    elif dataset_name == 'corr/pascal3d':
+    elif dataset_name == 'pascal3dCorr':
+        corruption = {'corruption': 'all', 'severity': 5}
         ds = pascal3d.Pascal3d(root=root, target_type="category", transform=transform, download=False,
-                               split=split, corruption={'corruption': 'all', 'severity': 5}, **kwargs)
+                               split=split, corruption=corruption, **kwargs)
     elif dataset_name == "oodcv":
         ds = oodcv.OODCV(root=root, target_type="category", transform=transform, download=False,
                                split=split, **kwargs)
